47803f5f2be845e2e9946508d0e4b11636dbb5ed
jp9621 <107367682+jp9621@users.noreply.github.com>
2023-07-01 21:38:24 -0500

    Update stock.cpp

--- stock.cpp
+++ stock.cpp
@@ -1,15 +1,27 @@
-//partial solution
-
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxprofit = 0;
-        for (int i = 0; i < prices.size(); i++) {
-            int pricedif = prices[i+1] - prices[i];
-            if (prices[i] < prices[i+1] && pricedif > maxprofit) {
-                maxprofit = pricedif;
+        if(prices.size() == 1){
+            return 0;
+        }
+        // Create window with size of two index values
+        int l = 0;
+        int r = 1;
+        // Initial profit is 0
+        int mp = 0;
+        
+        while(r < prices.size()){
+            // If the left index is greater than the right, the price is decreasing and no profit can be made, so slide the window down by one value
+            if(prices[l] > prices[r]){
+                l = r;
+                r++;
+            // If the right index is greater than the left, the price is increasing; expand the window by tethering the left boundary while advancing to right boundary, checking at every index the maximum profit
+            }else{
+                mp = max(mp, prices[r] - prices[l]);
+                r++;
             }
         }
-        return maxprofit;
+        
+        return mp;
     }
 };
